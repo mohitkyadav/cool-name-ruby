1b60b3c7f5bea00a5b6eab5e3d333d02f86dd907
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2025-03-16 20:11:07 +0100

    Create char_mappings.rb

---
+++ lib/cool_name_gen/char_mappings.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# CoolNameGen module
+module CoolNameGen
+  # CharMappings module
+  module CharMappings
+    CHAR_TO_NUM_MAP = {
+      "a" => "4",
+      "b" => "6",
+      "e" => "3",
+      "g" => "9",
+      "i" => "!",
+      "l" => "1",
+      "o" => "0",
+      "s" => "5",
+      "z" => "2",
+      " " => "_"
+    }.freeze
+  end
+end
